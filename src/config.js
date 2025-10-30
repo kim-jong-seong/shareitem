1b7e704741da2816984f1e235abf3d3b6cbf6e79
kim-jong-seong <ydh1178@naver.com>
2025-10-30 14:11:32 +0900

    크로스운영체제 포트설정 적용

--- src/config.js
+++ src/config.js
@@ -1,5 +1,1 @@
-// 환경 변수에서 가져오기
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
-
-// 디버깅용
-console.log('🔗 API URL:', API_URL);
+export const API_URL = 'http://18.219.127.156:3001';
